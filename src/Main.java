c83c22a46ba9d0503b46d86866323df3ffa186f1
Al01z <dampc.artur@gmail.com>
2022-01-22 13:46:01 +0100

    cw4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main {
 
@@ -20,6 +21,31 @@ public class Main {
            boolean isIndexCorrect
            white(!isIndexCorrect) {}
         */
+        boolean isIndexCorrect;
+        String wejscie = "";
+        int index = -1;
+        isIndexCorrect = false;
+        int[] tablica = {2,56,58,31,44,256};
+        Scanner scanner = new Scanner(System.in);
+        while(!isIndexCorrect) {
+            try {
+                System.out.println("Podaj indeks do wyświetlenia:");
+                System.out.println(tablica[Integer.parseInt(scanner.next())]);
+                isIndexCorrect = true;
+                System.out.println("Podaj indeks do wyświetlenia:");
+            }
+            catch (NumberFormatException e) {
+                    System.out.println("Błąd, wprowadzony znak nie jest cyfrą!");
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Błąd Podałeś index: " + index + ", a dozwolone są od 0 do " + (tablica.length - 1));
+            }
+            finally {
+                System.out.println("Koniec Programu.");
+            }
+        }
+
+
 
 
 
